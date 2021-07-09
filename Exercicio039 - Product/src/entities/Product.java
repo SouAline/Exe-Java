722110d16a7a1bb9b68f2f2a9cba8088204d8882
Aline Souza <ds.alinesouza@gmail.com>
2021-07-09 11:21:36 -0300

    Arruma erro de inglês e remove comentário

--- Exercicio039 - Product/src/entities/Product.java
+++ Exercicio039 - Product/src/entities/Product.java
@@ -18,11 +18,26 @@ public class Product {
 	product.name = sc.nextLine();
 	System.out.print("Price R$: ");
 	product.price = sc.nextDouble();
-	System.out.print("Quantity in stock: ");
-	product.quantity = sc.nextInt();// this variable is declareted with teclado
+	System.out.println("Quantity in stock: ");
+	product.quantity = sc.nextInt();
+	
+	System.out.println(product.name + " - " + product.price + " - " + product.quantity);
+
+	System.out.println();
+	System.out.println("Enter the number of products added in stock: ");
+	int quantity = sc.nextInt();
+	product.addProducts(quantity);
+	System.out.println("Update data: " + product);
+	
+	System.out.println();
+	System.out.println("Enter the number of products to be removed from stock: ");
+	quantity = sc.nextInt();
+	product.removeProducts(quantity);
+	System.out.println("Update data: " + product);
 	
 	System.out.println(product.name + " - " + product.price + " - " + product.quantity);
 	sc.close();
+			
 	}
 
 }
