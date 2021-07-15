0fc39072c56939cf2d46c7423512d1c21289a1a0
Aline Souza <ds.alinesouza@gmail.com>
2021-07-14 23:46:26 -0300

    Exercicio simples para pratica da linguagem

---
+++ Exercicio042 - Arrays/src/ImprimindoMatrizes.java
@@ -0,0 +1,14 @@
+
+public class ImprimindoMatrizes {
+	public static void main (String[] args) {
+	char [][] letras = new char[2][3];
+	 letras [0][0] = 'a';
+	 letras [0][1] = 'b';
+	 letras [0][2] = 'c';
+	 letras [1][0] = 'd';
+	 letras [1][1] = 'e'; 
+	 letras [1][2] = 'f';
+	 System.out.println(letras[0][0] +"|" + letras[0][1]+"|" + letras[0][2]);
+	}
+	
+}
